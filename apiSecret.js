69ecc08008e4b7354783f5c23a4e67beec462c45
davos666 <dave.otterspeer@gmail.com>
2024-03-02 09:53:53 +0100

    Add files via upload

---
+++ apiSecret.js
@@ -0,0 +1,11 @@
+// apiSecret.js
+
+// vul hier je api en secret in 
+const API_KEY = 'vul hier je api in';
+const API_SECRET = 'vul hier je secret in';
+
+// Export them
+module.exports = {
+    apiKey: API_KEY,
+    apiSecret: API_SECRET
+};
---
+++ walet.js
@@ -0,0 +1,32 @@
+const { apiKey, apiSecret } = require('./apiSecret');
+const { RestClientV5 } = require('bybit-api');
+const args = process.argv.slice(2);
+if (args.length < 2) {
+    console.log("coin is "+args[0]);
+}
+
+
+const client = new RestClientV5({
+  testnet: false,
+  key: apiKey,
+  secret: apiSecret,
+});
+
+
+client
+  .getWalletBalance({
+    accountType: 'SPOT',
+    coin: args[0], 
+  })
+  .then((response) => {
+
+console.log(response.result.list[0].coin[0]);
+
+    });
+
+
+
+
+
+
+
